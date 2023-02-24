33f95dced0be7a84eaed14ccb82da51e302b9e8d
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2023-02-24 14:16:39 -0500

    Deploy React app to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Wii</code> Project Hub.
         </p>
         <a
           className="App-link"
