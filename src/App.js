b1b01fedb86d30ea631ec0fac620a2d0c60fd6a5
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2023-02-25 19:45:14 -0500

    Added Houdini channel shapes, some basic channels and their images.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,54 @@ function App() {
   const d = new Date();
   let dateDisplay = weekday[d.getDay()] + " " + (d.getMonth() + 1) + "/" + d.getDate();
 
+  // https://github.com/PavelLaptev/squircle-houdini-css
+  if ("paintWorklet" in CSS) {
+    CSS.paintWorklet.addModule(
+      "https://www.unpkg.com/css-houdini-squircle/squircle.min.js"
+    );
+  }
+
   return (
     <div className="App">
       <div className="content">
-        <a className="squircle" href="https://booper1.github.io/" target="_blank" rel="noreferrer">
-          <img src={logo} className="logo" alt="logo" />
-        </a>
-
-        <h1>Under Contruction.<br/>Please come back later.</h1>
+        <div className="channels">
+          <div class="link link_fill link_outline booper">
+            <a href="https://booper1.github.io/" target="_blank" rel="noreferrer">
+              <img src={logo} className="myLogo" alt="Booper_1 logo" />
+            </a>
+          </div>
+          <div class="link link_fill link_outline mii">
+          </div>
+          <div class="link link_fill link_outline photo">
+          </div>
+          <div class="link link_fill link_outline shop">
+          </div>
+          <div class="link link_fill link_outline homebrew">
+          </div>
+          <div class="link link_fill link_outline">
+          </div>
+          <div class="link link_fill link_outline">
+          </div>
+          <div class="link link_fill link_outline">
+          </div>
+          <div class="link link_fill link_outline">
+          </div>
+          <div class="link link_fill link_outline">
+          </div>
+          <div class="link link_fill link_outline">
+          </div>
+          <div class="link link_fill link_outline">
+          </div>
+        </div>
       </div>
       
       <div className="footer">
+        <h1>Under Contruction.<br/>Please come back later.</h1>
         <p className="date">{dateDisplay}</p>
       </div>
     </div>
+
+    
   );
 }
 
